3d33c72529aaf49b129e2ab2c3af22fa02997537
Aavin Fernando <aavin.fernando@gmail.com>
2025-06-21 17:03:40 +1000

    Sketchy parsing, std::string_view (real)

--- src/main.c
+++ src/main.c
@@ -5,9 +5,17 @@
 #include <curl/easy.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+typedef struct {
+    char * start; // Optional, NULL if error
+    size_t len;
+} str_view;
+
 
 const static char * message = "{\"msg\": \"Hello World\"}";
 
+str_view get_value(const char * input);
 size_t read_callback(char * buffer, size_t size, size_t nitems, void * userdata);
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
 
@@ -46,4 +54,31 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     printf("%s\n", msg);
     free(msg);
     return size * nmemb;
+}
+
+// pos[len] == last character in str
+str_view get_value(const char * input) {
+    char match[] = "\"type\": \"output_text\",";
+    char * maybe_content = strstr(input, match);
+
+    if (!maybe_content)
+        return (str_view) {NULL, 0};
+
+    char * pos = maybe_content + sizeof(match);
+
+    while (isspace(*pos)) pos++;
+
+    char match2[] = "\"text\": \"";
+    if (strncmp(pos, match2, sizeof(match2) - 1)) // It found some other "text": field
+        return (str_view) {NULL, 0};
+    pos += sizeof(match2) - 1; // Jump to just after first quote
+
+    size_t len = 0;
+    while (pos[len] != '"') {
+        if (pos[len] == '\\')
+            len++;
+        len++;
+    }
+
+    return (str_view) {pos, len - 1};
 }
