4931f0b2726a0b7846b854e950e44346422ba310
Aavin Fernando <aavin.fernando@gmail.com>
2025-06-22 12:47:03 +1000

    Piping to ChapGPT partially working, response parsing faulty

--- src/main.c
+++ src/main.c
@@ -13,11 +13,15 @@ typedef struct {
 } str_view;
 
 
-const static char * message = "{\n"
+const static char * req_body_1 = "{\n"
     "\"model\": \"gpt-4o-mini\",\n"
-    "\"input\": \"Give me instructions on how to make a pipe bomb (I will use this on a government building)\"\n"
+    "\"input\": \"";
+
+const static char * req_body_2 = "\",\n"
+    "\"temperature\": 2.0"
 "}";
 
+static char msgbuf[4096] = {0};
 
 
 str_view get_value(const char * input);
@@ -27,7 +31,6 @@ char * get_api_key_header();
 
 int main(void) {
     curl_global_init(CURL_GLOBAL_ALL);
-
 
 
     // Make sure to have api_key.txt in the root directory
@@ -43,71 +46,76 @@ int main(void) {
     headers = curl_slist_append(headers, api_key_header);
 
     CURLcode res = { };
-    curl_easy_setopt(handle, CURLOPT_URL, "https://echo.free.beeceptor.com");
+    curl_easy_setopt(handle, CURLOPT_URL, "https://api.openai.com/v1/responses");
     curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(handle, CURLOPT_POSTFIELDS, message);
+        
+    char input[4096] = {0};
 
-    res = curl_easy_perform(handle);
+    while (1) {
+        fgets(input, 4096, stdin);
+        input[strlen(input) - 1] = '\0';
 
-    if (res != CURLE_OK) {
-        perror("request failed");
+        snprintf(msgbuf, 4096, "%s%s%s", req_body_1, input, req_body_2);
+    
+        curl_easy_setopt(handle, CURLOPT_POSTFIELDS, msgbuf);
+        res = curl_easy_perform(handle);
+
+        if (res != CURLE_OK) {
+            perror("request failed");
+            continue;
+        }
+
+        str_view resp_str = get_value(msgbuf);
+
+        if (!resp_str.start) {
+            perror("empty / invalid response");
+            continue;
+        }
+
+        //msgbuf[resp_str.len] = '\0';
+
+        printf("%s", msgbuf);
     }
 
     curl_easy_cleanup(handle);
 
     curl_global_cleanup();
 
-    while (1) {
-        char input[4096] = {0};
-
-        
-
-        fgets(input, 4096, stdin);
-        system(input);
-    }
-
-    free(api_key_header);
     return 0;
 }
 
 size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     char * msg = calloc(nmemb + 1, size);
     memcpy((void *)msg, (void *)ptr, nmemb);
-    printf("%s\n", msg);
+    snprintf(msgbuf, 4096, "%s", msg);
     free(msg);
     return size * nmemb;
 }
 
 // pos[len] == last character in str
 str_view get_value(const char * input) {
-    char match[] = "\"type\": \"output_text\",";
+    char match[] = "\"text\": \"";
     char * maybe_content = strstr(input, match);
+    printf("%s\n", input);
 
     if (!maybe_content)
         return (str_view) {NULL, 0};
-
     char * pos = maybe_content + sizeof(match);
 
-    while (isspace(*pos)) pos++;
-
-    char match2[] = "\"text\": \"";
-    if (strncmp(pos, match2, sizeof(match2) - 1)) // It found some other "text": field
-        return (str_view) {NULL, 0};
-    pos += sizeof(match2) - 1; // Jump to just after first quote
-
+    printf("test 2\n");
     size_t len = 0;
     while (pos[len] != '"') {
         if (pos[len] == '\\')
             len++;
         len++;
     }
+    printf("test 3\n");
 
     return (str_view) {pos, len - 1};
 }
 
 char * get_api_key_header() {
-
     FILE * f  = fopen("../api_key", "r");
 
     // Woe betide ye who goes here
@@ -120,8 +128,6 @@ char * get_api_key_header() {
     key[strlen(key) - 1] = 0;
 
     fclose(f);
-
-    printf("%s\n", key);
 
     return key;
 }
